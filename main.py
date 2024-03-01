597cdf5125b700b5e4693ed27e397bace4623545
alberts <alberts101@163.com>
2024-03-01 10:41:28 +0800

    first card for base64 convert realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from tools.encoding import encode_to_base64
+from tools.encoding import encode_to_base64, decode_from_base64
 
 app = Flask(__name__)
 
@@ -7,13 +7,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/tool', methods=['GET', 'POST'])
-def tool():
+@app.route('/base64', methods=['GET', 'POST'])
+def base64():
     if request.method == 'POST':
-        text = request.form.get('text', '')
-        encoded_text = encode_to_base64(text)
-        return render_template('tool.html', text=text, encoded_text=encoded_text)
-    return render_template('tool.html')
+        text_encode = request.form.get('text_encode', '')
+        encoded_text = encode_to_base64(text_encode)
+
+        encoded_text_decode = request.form.get('encoded_text_decode', '')
+        text_decode = decode_from_base64(encoded_text_decode)
+
+        return render_template('base64.html',
+                                text_encode=text_encode,
+                                encoded_text=encoded_text,
+                                encoded_text_decode=encoded_text_decode,
+                                text_decode=text_decode)
+    return render_template('base64.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9205)
--- tools/encoding.py
+++ tools/encoding.py
@@ -3,3 +3,7 @@ import base64
 def encode_to_base64(text):
     encoded_text = base64.b64encode(text.encode()).decode()
     return encoded_text
+
+def decode_from_base64(encoded_text):
+    decoded_text = base64.b64decode(encoded_text).decode()
+    return decoded_text
