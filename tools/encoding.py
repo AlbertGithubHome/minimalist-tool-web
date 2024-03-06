ad9cef71683be1a6ede3eda533e4075eb016c8e1
alberts <alberts101@163.com>
2024-03-06 14:15:35 +0800

    support chinese character

--- tools/encoding.py
+++ tools/encoding.py
@@ -14,6 +14,12 @@ def string_escape(s, encoding='utf-8'):
              .encode('latin1')         # 1:1 mapping back to bytes
              .decode(encoding))        # Decode original encoding
 
+def comm_string_escape(s, encoding='utf-8'):
+    return (s.encode('utf-8')          # To bytes, required by 'unicode-escape'
+             .decode('unicode-escape') # Perform the actual octal-escaping decode
+             .encode('latin1')         # 1:1 mapping back to bytes
+             .decode(encoding))        # Decode original encoding
+
 def local_bytes_str_to_text(bytes_str):
     text_result = bytes_str.encode('latin1').decode('utf-8')
     return text_result
@@ -22,18 +28,19 @@ def text_to_bytes_str(text):
     bytes_result = text.encode('utf-8')
     return str(bytes_result)
 
-#print(bytes_str_to_text('\xe5\xbe\x88\xe6\xa3\x92'))
-#print(bytes_str_to_text('\346\240\274'))
+#print(bytes_str_to_text(r'\xe5\xbe\x88\xe6\xa3\x92'))
+#print(bytes_str_to_text(r'\346\240\274'))
+#print(local_bytes_str_to_text('\346\240\274'))
 def bytes_str_to_text(bytes_str):
-    return string_escape(bytes_str)
+    return comm_string_escape(bytes_str)
 
 def hex_with_dash(string):
     hex_string = ''.join(hex(ord(c))[2:].zfill(2) for c in string)
     return '-'.join(hex_string[i:i+2] for i in range(0, len(hex_string), 2))
 
 def xor_text(text1, text2):
-    bytes1 = text1.encode('latin1').decode('unicode-escape').encode('latin1')
-    bytes2 = text2.encode('latin1').decode('unicode-escape').encode('latin1')
+    bytes1 = text1.encode('utf-8').decode('unicode-escape').encode('latin1')
+    bytes2 = text2.encode('utf-8').decode('unicode-escape').encode('latin1')
 
     length = min(len(bytes1), len(bytes2))
 
